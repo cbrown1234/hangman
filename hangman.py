2145374702662414a4098f33e718a650c09c9e99
Chris Brown <chris@cfbrown.co.uk>
2019-04-27 13:45:15 +0100

    Improve game
    Add limit to misses
    Improve UI
    Add ability to check whole word

--- hangman.py
+++ hangman.py
@@ -11,25 +11,60 @@ words = [
 
 
 class Game:
-    def __init__(self, word: str):
-        self.word = word
-        self.known_letters = '_'*len(self.word)
+    max_misses = 8
 
-    def check_letter(self, letter: str) -> str:
-        known_letters_list = [
-            letter if letter == l1 else l2
-            for l1, l2 in zip(self.word, self.known_letters)
-        ]
-        self.known_letters = ''.join(known_letters_list)
+    def __init__(self, word: str):
+        self.word = word.lower()
+        self.known_letters = '_'*len(self.word)
+        self.guess = '?'
+        self.misses = []
+
+    @property
+    def ui(self):
+        return f"""
+Word: {self.known_letters}
+Guess: {self.guess}
+Misses ({len(self.misses)}/{self.max_misses}): {', '.join(self.misses)}
+"""
+
+    def check_letter(self, letter):
+        if letter in self.word:
+            known_letters_list = [
+                letter if letter == l1 else l2
+                for l1, l2 in zip(self.word, self.known_letters)
+            ]
+            self.known_letters = ''.join(known_letters_list)
+        else:
+            self.misses.append(letter)
         return self.known_letters
+
+    def check_word(self, word):
+        if self.word == word:
+            self.known_letters = word
+        else:
+            self.misses.append(word)
+
+    def check_guess(self):
+        if self.guess in self.misses:
+            print(f'Already tried {self.guess}')
+        else:
+            if len(self.guess) == 1:
+                self.check_letter(self.guess)
+            else:
+                self.check_word(self.guess)
 
     def play(self):
         print('Starting Hangman')
         print(f'The word has {len(self.word)} letters: {self.known_letters}')
-        while self.word != self.known_letters:
-            letter = input('Next letter?')
-            print(self.check_letter(letter))
-        print('Well Done')
+        while len(self.misses) < self.max_misses:
+            self.guess = str(input('Next guess?')).lower()
+            self.check_guess()
+            print(self.ui)
+
+        if self.known_letters == self.word:
+            print('Well Done!')
+        else:
+            print(f'Bad luck, the word was: {self.word}')
 
 
 def main():
